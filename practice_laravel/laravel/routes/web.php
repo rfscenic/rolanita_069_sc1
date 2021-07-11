b74988bab72f9ca2ff6e5e1b59a7254ea6cbf2a6
ROLANITA SCENIC <18102069@ittelkom-pwt.ac.id>
2021-07-11 07:04:40 +0700

    Practice12 - Login

--- practice_laravel/laravel/routes/web.php
+++ practice_laravel/laravel/routes/web.php
@@ -23,3 +23,7 @@ Route::get('/student/{student}', 'StudentController@show') ->name('student.show'
 Route::get('/student/{student}/edit', 'StudentController@edit') ->name('student.edit');
 Route::patch('/student/{student}', 'StudentController@update') ->name('student.update');
 Route::delete('/student/{student}', 'StudentController@destroy') ->name('student.destroy');
+
+Route::get('/login', 'AdminController@index')->name('login.index');
+Route::get('/logout', 'AdminController@logout')->name('login.logout');
+Route::post('/login', 'AdminController@process')->name('login.process');
